8461b3903cd717ec552f450896e0f1ee9de9659f
michal-swiatek <swiatek.michal006@gmail.com>
2020-03-31 14:33:22 +0200

    Added simple AI (random moves)

--- checkers/player.py
+++ checkers/player.py
@@ -4,22 +4,28 @@
     Last update: 30.03.2020
 """
 
+import random
+
 
 class Player:
 
     def __init__(self, color: bool):
         self.color = color
 
-        self.held_piece = None
-
-    def makeMove(self, board):
+    # Complete game info
+    def makeMove(self, board, held_piece=None, captured_pieces=None):
         pass
 
 
 class Human(Player):
 
-    def makeMove(self, board):
-        if self.held_piece is not None:
+    def __init__(self, color: bool):
+        super(Human, self).__init__(color)
+
+        self.held_piece = None
+
+    def makeMove(self, board, held_piece=None, captured_pieces=None):
+        if held_piece is None and self.held_piece is not None:
             piece = self.held_piece
             radius = int(board.tile_size / 2)
 
@@ -30,4 +36,34 @@ class Human(Player):
 
             return players_move
         else:
+            self.held_piece = held_piece
             return None
+
+
+class SimpleAI(Player):
+
+    def makeMove(self, board, held_piece=None, captured_pieces=None):
+        pieces = board.getPieces(self.color)
+        bitmap = board.getBoardBitmap(captured_pieces, self.color)
+
+        moves = self.generateValidMoves(pieces, bitmap)
+        move = random.choice(moves)
+
+        return move
+
+    def generateValidMoves(self, pieces, bitmap):
+        valid_moves = []
+        valid_captures = []
+
+        for piece in pieces:
+            moves, captures = piece.generatePossibleMoves(bitmap, len(valid_captures) != 0)
+
+            if len(captures) != 0:
+                valid_captures.extend(captures)
+            else:
+                valid_moves.extend(moves)
+
+        if len(valid_captures) != 0:
+            valid_moves = valid_captures
+
+        return valid_moves
