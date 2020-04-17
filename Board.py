05a741855f1f2fec365d9240f6e8fea008212762
michal-swiatek <swiatek.michal006@gmail.com>
2020-04-18 00:55:34 +0200

    Changed piece display routine

--- Board.py
+++ Board.py
@@ -13,29 +13,36 @@ class Board:
         for y in range(3):
             for x in range(8):
                 if (x + y) % 2 == 0:
-                    self.white_pieces.append(Man(Piece.WHITE, y, x))
+                    self.white_pieces.append(Man(Piece.WHITE, x, y))
 
         # Black pieces
         for y in range(5, 8):
             for x in range(8):
                 if (x + y) % 2 == 0:
-                    self.white_pieces.append(Man(Piece.BLACK, y, x))
+                    self.black_pieces.append(Man(Piece.BLACK, x, y))
+
+    def getPieces(self, color):
+        if color == Piece.WHITE:
+            return self.white_pieces
+        else:
+            return self.black_pieces
 
     def generateBitmap(self):
         """Creates board bitmap"""
         bitmap = [[None for columns in range(8)] for rows in range(8)]
 
-        for piece in self.white_pieces:
-            if not piece.captured:
-                bitmap[piece.column][piece.row] = piece.white
-            else:
-                bitmap[piece.column][piece.row] = not piece.white
+        for white_piece, black_piece in itertools.zip_longest(self.white_pieces, self.black_pieces):
+            if white_piece is not None:
+                if not white_piece.captured:
+                    bitmap[white_piece.x][white_piece.y] = white_piece.white
+                else:
+                    bitmap[white_piece.x][white_piece.y] = not white_piece.white
 
-        for piece in self.black_pieces:
-            if not piece.captured:
-                bitmap[piece.column][piece.row] = piece.white
-            else:
-                bitmap[piece.column][piece.row] = not piece.white
+            if black_piece is not None:
+                if not black_piece.captured:
+                    bitmap[black_piece.x][black_piece.y] = black_piece.white
+                else:
+                    bitmap[black_piece.x][black_piece.y] = not black_piece.white
 
         return bitmap
 
@@ -45,9 +52,10 @@ class Board:
 
         for white_piece, black_piece in itertools.zip_longest(self.white_pieces, self.black_pieces):
             if white_piece is not None:
-                grid[white_piece.column][white_piece.row] = white_piece
+                grid[white_piece.x][white_piece.y] = white_piece
+
             if black_piece is not None:
-                grid[black_piece.column][black_piece.row] = black_piece
+                grid[black_piece.x][black_piece.y] = black_piece
 
         return grid
 
@@ -58,7 +66,7 @@ class Board:
 
         c = 0
         r = 7
-        print("\n", "  ###################", end='')
+        print("  ###################", end='')
         while r >= 0:
             c = 0
             print("\n", r, "#", end='')
@@ -69,21 +77,21 @@ class Board:
                     else:
                         print(" .", end='')
                 elif grid[c][r].captured:
-                    print(" *", end='')
-                elif not grid[c][r].white:
-                    print(" %", end='')
-                elif grid[c][r].white:
                     print(" @", end='')
+                else:
+                    print(' ', grid[c][r].displayCharacter(), sep='', end='')
                 c = c + 1
             print(" #", end='')
             r = r - 1
-        print("\n", "  ###################", "\n", "    0 1 2 3 4 5 6 7", "\n")
+        print("\n", "  ###################", "\n", "    0 1 2 3 4 5 6 7")
 
     def clear_captured(self):
         """Removes captured pieces from the board"""
 
-        pass    # TODO: Implement
+        for i in range(len(self.white_pieces) - 1, -1, -1):
+            if self.white_pieces[i].captured:
+                self.white_pieces.remove(self.white_pieces[i])
 
-
-#------------------------------------------------------------------------------------------------------------------------------------------
-
+        for i in range(len(self.black_pieces) - 1, -1, -1):
+            if self.black_pieces[i].captured:
+                self.black_pieces.remove(self.black_pieces[i])
