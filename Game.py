4caca9eb7e61b01a4c3c28861792a543d6aadef3
michal-swiatek <swiatek.michal006@gmail.com>
2020-04-18 00:55:56 +0200

    Moved game into class

--- Game.py
+++ Game.py
@@ -3,116 +3,89 @@ import Pieces
 import Players
 
 
-def generateValidMoves(board):
-    valid_moves, valid_captures = [], []
-    board_bitmap = board.generateBitmap()
+class Game:
+    """ Game instance """
 
-    for piece in board.white_pieces:
-        moves, captures = piece.generateValidMoves(board_bitmap, len(valid_captures) != 0)
+    def __init__(self):
+        self.running = True
 
-        if len(captures) != 0:
-            valid_captures.extend(captures)
-        else:
-            valid_moves.extend(moves)
+        self.board = Board.Board()
 
-    for piece in board.black_pieces:
-        moves, captures = piece.generateValidMoves(board_bitmap, len(valid_captures) != 0)
+        self.white_player = Players.Human(Pieces.Piece.WHITE)
+        self.black_player = Players.Human(Pieces.Piece.BLACK)
 
-        if len(captures) != 0:
-            valid_captures.extend(captures)
-        else:
-            valid_moves.extend(moves)
+        self.current_player = Pieces.Piece.WHITE
 
-    if len(valid_captures) != 0:
-        return valid_captures
-    else:
-        return valid_moves
+        self.capturing_piece = None
 
+    def mainLoop(self):
+        """ Game loop """
 
-next_move = None
-current_player = Pieces.Piece.WHITE
-Table = Board.Board()
-white_player = Players.Player()
-black_player = Players.Player()
-active_piece_row = None
-active_piece_column = None
-
-#Main game loop
-
-while True:
-    #break       #for debug purposes
-    if current_player == Pieces.Piece.WHITE:
-        next_move = white_player.pass_control(Table)
-    elif current_player == Pieces.Piece.BLACK:
-        next_move = black_player.pass_control(Table)
-
-    if next_move == "surrender":
-        if current_player == Pieces.Piece.WHITE:
-            print("Black player wins! (White surrendered)")
-            break
-        elif current_player == Pieces.Piece.BLACK:
-            print("White player wins! (Black surrendered)")
-    else:
-        valid_moves = generateValidMoves(Table)
-
-        if next_move in valid_moves:
-            origin_row = int(next_move[1])          # y
-            origin_column = int(next_move[0])       # x
-            destination_row = int(next_move[3])     # y
-            destination_column = int(next_move[2])  # x
-
-            #check for capture
-            if abs(destination_row - origin_row) == 1:
-                #no capture
-                Table.grid[origin_row][origin_column].row = destination_row
-                Table.grid[origin_row][origin_column].column = destination_column
-                Table.grid[destination_row][destination_column] = Table.grid[origin_row][origin_column]
-                Table.grid[origin_row][origin_column] = None
-
+        while self.running:
+            if self.current_player == Pieces.Piece.WHITE:
+                next_move = self.white_player.pass_control(self.board, self.capturing_piece)
             else:
-                #with capture
-                capture_row = (origin_row + int((destination_row - origin_row)/2))
-                capture_column = (origin_column + int((destination_column - origin_column)/2))
+                next_move = self.black_player.pass_control(self.board, self.capturing_piece)
 
-                Table.grid[origin_row][origin_column].row = destination_row
-                Table.grid[origin_row][origin_column].column = destination_column
-                Table.grid[destination_row][destination_column] = Table.grid[origin_row][origin_column]
-                Table.grid[origin_row][origin_column] = None
+            if next_move == "surrender":
+                if self.current_player == Pieces.Piece.WHITE:
+                    print("Black player wins! (White surrendered)")
+                else:
+                    print("White player wins! (Black surrendered)")
 
-                Table.grid[capture_row][capture_column].alive = 0       #stays on board in case of chain capture
+                self.running = False
+            elif next_move == "game over":
+                if self.current_player == Pieces.Piece.WHITE:
+                    print("Black player wins!")
+                else:
+                    print("White player wins!")
 
-                Table.display()     #for debug purposes
+                self.running = False
+            else:
+                self.updateBoard(next_move)
 
-                #if()       ---  check for chain capture
-                #   active_piece_row = destination_row
-                #   active_piece_column = destination_column
-                #   continue - validation function will take active piece into account
+                if self.capturing_piece is None:
+                    self.current_player = not self.current_player
 
-                #else       --- no chain capture
-                Table.clear_captured()
-                active_piece_row = None
-                active_piece_column = None
-                if current_player == Pieces.Piece.WHITE:
-                    current_player = Pieces.Piece.BLACK
-                elif current_player == Pieces.Piece.BLACK:
-                    current_player = Pieces.Piece.WHITE
+    def updateBoard(self, move):
+        x1, y1, x2, y2, capture_x, capture_y = move
+        pieces = self.board.getPieces(self.current_player)
 
+        for i, piece in enumerate(pieces):
+            if x1 == piece.x and y1 == piece.y:
+                # Promote piece
+                if (piece.white and y2 == 7) or (not piece.white and y2 == 0):
+                    pieces[i] = Pieces.King(piece.white, piece.x, piece.y)
 
+                # Move piece to new position
+                pieces[i].x = x2
+                pieces[i].y = y2
 
+                # Capture occurred
+                if capture_x is not None:
+                    self.capturePiece(capture_x, capture_y)
 
-#for debug purposes
+                    # Chain capture condition
+                    if self.checkPossibleCaptures(pieces[i]):
+                        self.capturing_piece = pieces[i]
+                    else:
+                        self.capturing_piece = None
+                        self.board.clear_captured()
 
+                break
 
-temp = Table.generateBitmap()
-print("\n", temp[7], "\n", temp[6], "\n", temp[5], "\n", temp[4], "\n", temp[3], "\n", temp[2], "\n", temp[1], "\n", temp[0], "\n")     #for debug purposes
+    def capturePiece(self, piece_x, piece_y):
+        pieces = self.board.getPieces(not self.current_player)
 
-print(Table.grid[7][1].owner)   #for debug purposes
+        for i, piece in enumerate(pieces):
+            if piece_x == piece.x and piece_y == piece.y:
+                pieces[i].captured = True
 
-Table.display()     #for debug purposes
+    def checkPossibleCaptures(self, piece):
+        board_bitmap = self.board.generateBitmap()
+        moves, captures = piece.generateValidMoves(board_bitmap, True)
 
-
-
-
-
-next_move = white_player.pass_control(Table)
-print(next_move)
+        if len(captures) != 0:
+            return piece
+        else:
+            return None
