736fc91940f2f045c77e22a0bf4cf80de7529a77
michal-swiatek <swiatek.michal006@gmail.com>
2020-04-20 18:45:28 +0200

    Changed starting menu

--- Game.py
+++ Game.py
@@ -145,7 +145,9 @@ class Game:
         while True:
             print("0. Human vs Bot (heuristic 1)")
             print("1. Human vs Bot (heuristic 2)")
-            print("2. Bot vs Bot (heuristic 2 vs 1)")
+            print("2. Human vs Bot (heuristic 1 vs 2)")
+            print("3. Human vs Bot (heuristic 1 vs 1)")
+            print("4. Human vs Bot (heuristic 2 vs 2)")
 
             choice = input("Enter choice: ")
             depth = input("Enter search depth: ")
@@ -154,8 +156,8 @@ class Game:
                 choice = int(choice)
                 depth = int(depth)
 
-                if choice < 0 or choice > 2:
-                    raise ValueError("Please enter choice in range 0-2")
+                if choice < 0 or choice > 4:
+                    raise ValueError("Please enter choice in range 0-4")
 
                 if depth < 1:
                     raise ValueError("Depth must be a positive integer")
@@ -171,7 +173,13 @@ class Game:
                 self.white_player = Players.Human(Pieces.Piece.WHITE)
                 self.black_player = Players.MinMaxBot(Pieces.Piece.BLACK, depth, heuristics.h2)
             elif choice == 2:
-                self.white_player = Players.MinMaxBot(Pieces.Piece.WHITE, depth, heuristics.h2_white)
+                self.white_player = Players.MinMaxBot(Pieces.Piece.WHITE, depth, heuristics.h1_white)
+                self.black_player = Players.MinMaxBot(Pieces.Piece.BLACK, depth, heuristics.h2)
+            elif choice == 3:
+                self.white_player = Players.MinMaxBot(Pieces.Piece.WHITE, depth, heuristics.h1_white)
                 self.black_player = Players.MinMaxBot(Pieces.Piece.BLACK, depth, heuristics.h1)
+            elif choice == 4:
+                self.white_player = Players.MinMaxBot(Pieces.Piece.WHITE, depth, heuristics.h2_white)
+                self.black_player = Players.MinMaxBot(Pieces.Piece.BLACK, depth, heuristics.h2)
 
             break
