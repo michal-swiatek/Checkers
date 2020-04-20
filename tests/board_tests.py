cc9daf9ce953a4405f7fe35a3f0b4fb1828c4218
michal-swiatek <swiatek.michal006@gmail.com>
2020-04-20 16:11:33 +0200

    Adding board unit tests

---
+++ tests/board_tests.py
@@ -0,0 +1,79 @@
+import unittest
+
+import Board
+
+
+class BoardTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.board = Board.Board()
+
+    def testConstructor(self):
+        white_pieces = self.board.getPieces(Board.Piece.WHITE)
+        black_pieces = self.board.getPieces(Board.Piece.BLACK)
+
+        # At the beginning of a game each player has 12 pieces
+        self.assertEqual(len(white_pieces), 12)
+        self.assertEqual(len(black_pieces), 12)
+
+        # Check that at the bottom left corner white piece is placed
+        grid = self.board.generateBoardState()
+        self.assertEqual(grid[0][0].x, 0)
+        self.assertEqual(grid[0][0].y, 0)
+        self.assertEqual(grid[0][0].white, Board.Piece.WHITE)
+
+    def testGenerateBitmap(self):
+        bitmap = self.board.generateBitmap()
+
+        bit_sum = 0
+        for x in range(8):
+            for y in range(8):
+                if bitmap[x][y] is True:
+                    bit_sum += 1
+                elif bitmap[x][y] is False:
+                    bit_sum -= 1
+
+        self.assertEqual(bit_sum, 0)  # board bitmap should be symmetric
+
+        # Setting one piece as captured should change its value in bitmap to not color
+        self.board.white_pieces[0].captured = True
+        bitmap = self.board.generateBitmap()
+
+        bit_sum = 0
+        for x in range(8):
+            for y in range(8):
+                if bitmap[x][y] is True:
+                    bit_sum += 1
+                elif bitmap[x][y] is False:
+                    bit_sum -= 1
+
+        self.assertEqual(bit_sum, -2)  # board bitmap after one capture should not be symmetric
+
+    def testGenerateBoardState(self):
+        grid = self.board.generateBoardState()
+
+        # Top right corner should contain black piece
+        self.assertEqual(grid[7][7].x, 7)
+        self.assertEqual(grid[7][7].y, 7)
+        self.assertEqual(grid[7][7].white, Board.Piece.BLACK)
+
+        # Bottom left corner should contain white piece
+        self.assertEqual(grid[0][0].x, 0)
+        self.assertEqual(grid[0][0].y, 0)
+        self.assertEqual(grid[0][0].white, Board.Piece.WHITE)
+
+        # Empty spot
+        self.assertEqual(grid[1][0], None)
+
+    def testClearCaptured(self):
+        grid = self.board.generateBoardState()
+        self.assertNotEqual(grid[0][0], None)
+
+        self.board.white_pieces[0].captured = True
+        self.board.clearCaptured()
+
+        grid = self.board.generateBoardState()
+        self.assertEqual(grid[0][0], None)
+
+
+if __name__ == "__main__":
+    unittest.main()
